4951eb857683e9b6bac324ccfe05085deafb9db0
Administrator <sk@unelma.ee>
2022-03-03 17:07:35 +0200

    Updated with Edit and other features

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,71 @@ import NewTodo from './components/NewTodo';
 import { Todo } from './todo.model';
 
 const App: React.FC = () => {
+  const [todoState, setTodoState] = useState<string>('add');
+  const [todo, setTodo] = useState<Todo>({ id: '', text: '' });
   const [todos, setTodos] = useState<Todo[]>([]);
-  const todoAddHandler = (text: string) => {
-    setTodos((prevTodos) => [
-      ...prevTodos,
-      { id: Math.random().toString(), text: text },
-    ]);
+
+  const onSubmitTodo = (text: string) => {
+    if (todoState === 'edit') {
+      setTodos((prevTodos) => {
+        return prevTodos.map((prevTodo) => {
+          if (prevTodo.id === todo.id) {
+            return todo;
+          } else {
+            return prevTodo;
+          }
+        });
+      });
+    } else {
+      setTodos((prevTodos) => [
+        ...prevTodos,
+        { id: Math.random().toString(), text: text },
+      ]);
+    }
+    setTodo((prevTodo) => {
+      return { ...prevTodo, id: '', text: '' };
+    });
+    setTodoState(() => {
+      return '';
+    });
+  };
+
+  const todoChangeHandler = (text: string) => {
+    setTodo((prevTodo) => {
+      return { ...prevTodo, text: text };
+    });
+  };
+
+  const todoEditHandler = (id: string) => {
+    setTodo((prevTodo) => {
+      const todo = todos.filter((todo) => todo.id === id);
+      const text = todo.length ? todo[0].text : '';
+      return { ...prevTodo, id: id, text: text };
+    });
+    setTodoState(() => {
+      return 'edit';
+    });
   };
 
   const todoDeleteHandler = (todoId: string) => {
     setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId);
+      return prevTodos.filter((prevTodo) => prevTodo.id !== todoId);
     });
   };
 
   return (
     <div className="App">
-      <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+      <NewTodo
+        text={todo.text}
+        state={todoState}
+        onChangeTodo={todoChangeHandler}
+        onSubmitTodo={onSubmitTodo}
+      />
+      <TodoList
+        items={todos}
+        onEditTodo={todoEditHandler}
+        onDeleteTodo={todoDeleteHandler}
+      />
     </div>
   );
 };
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,20 +4,22 @@ import './TodoList.css';
 
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onEditTodo: (id: string) => void;
   onDeleteTodo: (id: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
-  const todos = [{ id: 't1', text: 'Finish the course' }];
-
   return (
     <ul>
       {props.items.map((todo) => (
         <li key={todo.id}>
           <span>{todo.text}</span>
-          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
-            Delete
-          </button>
+          <div>
+            <button onClick={props.onEditTodo.bind(null, todo.id)}>Edit</button>
+            <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+              Delete
+            </button>
+          </div>
         </li>
       ))}
     </ul>
